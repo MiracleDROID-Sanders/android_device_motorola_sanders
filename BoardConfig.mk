fa64d2110d63d7226b88ba05de959659f02e7f13
syed sajid <syedsajid341@gmail.com>
2018-06-22 10:17:59 +0000

    Modified

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,10 +80,6 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := sanders_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 #
-# Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
-
 # SDCLANG
 TARGET_USE_SDCLANG := true
 
